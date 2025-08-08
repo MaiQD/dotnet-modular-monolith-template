6740187212fbb935e74dc52b0faed1c033e79d37
datmai <q.dat2710@gmail.com>
2025-08-09 06:22:37 +0700

    chore: remove Users module and related files from the project
    - Deleted all files and configurations related to the Users module.
    - Ensured that the API no longer includes endpoints or references to the Users module.
    - Updated project files and documentation to reflect the removal.

--- src/Acme.ModularMonolith/Modules/Users/App.Modules.Users.Application/Commands/LoginWithGoogleCommand.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-using App.Modules.Users.Application.DTOs;
-using App.SharedKernel.Results;
-using System.ComponentModel.DataAnnotations;
-
-namespace App.Modules.Users.Application.Commands;
-
-public record LoginWithGoogleCommand([Required] string GoogleToken) : IRequest<Result<LoginResponseDto>>;
--- src/Acme.ModularMonolith/Modules/Users/App.Modules.Users.Application/Queries/GetUserByIdQuery.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-using App.Modules.Users.Application.DTOs;
-using App.SharedKernel.Results;
-using System.ComponentModel.DataAnnotations;
-
-namespace App.Modules.Users.Application.Queries;
-
-public record GetUserByIdQuery(string UserId) : IRequest<Result<UserDto>>;
